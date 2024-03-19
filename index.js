4686afd67fdd69c2c970711f5ba659cbe2282f03
VINCENT VAN VOLSEM <vincent.vanvolsem@gmail.com>
2024-03-19 14:53:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const collegue = ["Benoit", "Alexandre", "Elias", "Emilie"];
+
+// for (let i = 0; i < collegue.length; i++) {
+//     console.log(`${collegue[i]}`);
+// }
+
+import chalk from 'chalk';
+console.log(chalk.blue('Benoit'));
+console.log(chalk.green('Alexandre'));
+console.log(chalk.red('Elias'));
+console.log(chalk.yellow('Emilie'));
